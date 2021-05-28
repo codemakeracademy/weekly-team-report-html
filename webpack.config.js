57b656c61c95a6bb1fdb7fae23e5c6839ea04bd9
NKaty <nd.katy@gmail.com>
2021-05-28 22:09:09 +0300

    Add invite team page

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ const config = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
+    new HtmlWebpackPlugin({
+      filename: "invite_team.html",
+      template: "invite_team.html",
+    }),
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
