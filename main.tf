33fa078161e608d3ae6a68984b584b7dcd7852f8
Shfarrukhb <61914081+Shfarrukhb@users.noreply.github.com>
2022-01-23 08:34:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,117 +1,87 @@
-resource "aws_s3_bucket" "b94959495" {
-  bucket = "mybucket94949595"
-  acl    = "public"
-
-  tags = {
-    Name = "My bucket"
-  }
+provider "aws" {
+      region     = "${var.region}"
+      access_key = "${var.access_key}"
+      secret_key = "${var.secret_key}"
 }
 
-locals {
-  s3_origin_id = "myS3Origin"
+resource "aws_s3_bucket" "ankodevopsfr" {
+  bucket                               = "${var.bucket_name}"
+  force_destroy                        = "${var.force_destroy}"
+  acl                                  = "public-read"
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.b94959495.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-    }
-  }
-
-  enabled             = true
+origin {
+    domain_name = "${aws_s3_bucket.ankodevopsfr.bucket_regional_domain_name}"
+    origin_id   = "my_first_origin"
+}
+enabled             = true
   is_ipv6_enabled     = true
   comment             = "Some comment"
-  default_root_object = "index.html"
-
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
-
-  aliases = ["mysite.example.com", "yoursite.example.com"]
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
+  default_root_object = "mypic"
+default_cache_behavior {
+    allowed_methods  = ["HEAD", "GET"]
+    cached_methods   = ["HEAD", "GET"]
+    target_origin_id = "my_first_origin"
+forwarded_values {
       query_string = false
-
-      cookies {
+cookies {
         forward = "none"
       }
     }
-
-    viewer_protocol_policy = "allow-all"
+viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
   }
-
-  # Cache behavior with precedence 0
+# Cache behavior with precedence 0
   ordered_cache_behavior {
     path_pattern     = "/content/immutable/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
+    target_origin_id = "my_first_origin"
+forwarded_values {
       query_string = false
       headers      = ["Origin"]
-
-      cookies {
+cookies {
         forward = "none"
       }
     }
-
-    min_ttl                = 0
+min_ttl                = 0
     default_ttl            = 86400
     max_ttl                = 31536000
     compress               = true
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
   }
-
-  # Cache behavior with precedence 1
+# Cache behavior with precedence 1
   ordered_cache_behavior {
     path_pattern     = "/content/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
+    target_origin_id  = "my_first_origin"
+forwarded_values {
       query_string = false
-
-      cookies {
+cookies {
         forward = "none"
       }
     }
-
-    min_ttl                = 0
+min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-    compress               = true
+    compress             = true
     viewer_protocol_policy = "redirect-to-https"
   }
-
-  price_class = "PriceClass_200"
-
-  restrictions {
+price_class = "PriceClass_200"
+restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      locations        = ["US", "CA", "GB", "IN"]
     }
   }
-
-  tags = {
+tags = {
     Environment = "production"
   }
-
-  viewer_certificate {
+viewer_certificate {
     cloudfront_default_certificate = true
   }
 }
