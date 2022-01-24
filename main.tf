be1b7138524c41f650e2b160b30143f57f494588
Shfarrukhb <61914081+Shfarrukhb@users.noreply.github.com>
2022-01-24 05:39:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
       region     = "${var.region}"
-      access_key = "${var.access_key}"
-      secret_key = "${var.secret_key}"
+      profile    = "ankodevopsfrmn"
 }
 
 resource "aws_s3_bucket" "ankodevopsfrmn" {
