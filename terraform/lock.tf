dc344d31ad17bec1a2cc11337fbb6cd94e8fedec
danielkhan-v1 <grigorovich314@gmail.com>
2022-01-26 12:53:37 +0100

    fix: terraform files

---
+++ terraform/lock.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "lab-dynomodb-daniil"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "lab-sthree-daniil"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    encrypt        = true
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -18,28 +18,6 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
-
-  attribute {
-    name = "lab-dynomodb-daniil"
-    type = "S"
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket = "lab-sthree-daniil"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    encrypt        = true
-  }
-}
-
 data "aws_iam_policy_document" "website_policy" {
   statement {
     actions = [
