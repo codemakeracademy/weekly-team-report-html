63eca06207b87e98a601a23e65e8e025b2f79c61
EC2 Default User <ec2-user@ip-172-16-10-142.us-west-2.compute.internal>
2022-01-26 13:19:53 +0000

    feat: add save tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
-resource "aws_dynamodb_table" "dynamodb-terraform-state-trogaev" {
-  name = "terraform-state-lock-trogaev"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
+#resource "aws_dynamodb_table" "dynamodb-terraform-state-trogaev" {
+#  name = "terraform-state-lock-trogaev"
+#  hash_key = "LockID"
+#  read_capacity = 20
+#  write_capacity = 20
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
 
 
-terraform {
-  backend "s3" {
-    bucket = "trogaev-bucket-lab"
-    #key    = "calabs/production/us-west-2/rslab/terraform.tfstate"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-state-lock-trogaev"
-    encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "trogaev-bucket-lab"
+#    #key    = "calabs/production/us-west-2/rslab/terraform.tfstate"
+#    key    = "terraform.tfstate"
+#    region = "us-west-2"
+#    dynamodb_table = "terraform-state-lock-trogaev"
+#    encrypt        = true
+#  }
+#}
 
 
 
