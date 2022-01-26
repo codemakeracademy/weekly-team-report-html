6145b61f0f316bd1d43797fcf2988670cee95779
EC2 Default User <ec2-user@ip-172-16-10-142.us-west-2.compute.internal>
2022-01-26 07:06:18 +0000

    fix: cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,10 +46,10 @@ EOF
   }
 }
 
-resource "aws_cloudfront_distribution" "trogaev_distribution" {
+resource "aws_cloudfront_distribution" "trogaev_distribution-lab" {
   origin {
-    domain_name = aws_s3_bucket.trogaev-bucket-1.bucket_regional_domain_name
-    origin_id   = "trogaev-origin"
+    domain_name = aws_s3_bucket.trogaev-bucket-1-lab.bucket_regional_domain_name
+    origin_id   = "trogaev-origin-lab"
 
     #s3_origin_config {
     #  origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
@@ -72,7 +72,7 @@ resource "aws_cloudfront_distribution" "trogaev_distribution" {
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "trogaev-origin"
+    target_origin_id = "trogaev-origin-lab"
 
     forwarded_values {
       query_string = false
