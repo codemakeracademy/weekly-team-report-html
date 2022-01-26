6a8e34aa7a06ab469825c0585159acc59cb5cebb
EC2 Default User <ec2-user@ip-172-16-10-142.us-west-2.compute.internal>
2022-01-26 12:53:56 +0000

    feat: add save tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,19 @@ terraform {
   }
 }
 
+resource "aws_dynamodb_table" "dynamodb-terraform-state-trogaev" {
+  name = "terraform-state-lock-trogaev"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
 provider "aws" {
   region  = "us-west-2" # Oregon
 #  profile = "PowerUserAccess-529396670287"
