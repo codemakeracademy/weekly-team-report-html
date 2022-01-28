dcf658abf7fa9f7015c1f9a54837947bcaf33dfa
Mikhail <92841571+mivoronkov@users.noreply.github.com>
2022-01-28 13:58:08 +0300

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -3,13 +3,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+      } 
   backend "s3" {
     bucket = "voronkov-bucket-remote-state"
     key    = "calabs/production/us-west-2/rslab/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraform-state-lock-voronkov"
     encrypt        = true
-  }    
+    
     }
   }
 }
